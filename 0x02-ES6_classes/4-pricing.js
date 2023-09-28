cf4f7c7954f036263e30f604ccc231710ba31dc1
allvo6377 <alvinpilot@gmail.com>
2023-09-28 22:53:04 +0000

    pricing-code-fix

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,49 +1,44 @@
-// 4-pricing.js
-import Currency from './3-currency.js';
+import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('Currency must be a Currency');
+    }
     this._amount = amount;
     this._currency = currency;
   }
 
-  // Getter and Setter for 'amount'
   get amount() {
     return this._amount;
   }
 
-  set amount(newAmount) {
-    if (typeof newAmount === 'number') {
-      this._amount = newAmount;
-    } else {
-      throw new Error('Invalid data type. Amount must be a number.');
-    }
-  }
-
-  // Getter and Setter for 'currency'
   get currency() {
     return this._currency;
   }
 
-  set currency(newCurrency) {
-    if (newCurrency instanceof Currency) {
-      this._currency = newCurrency;
-    } else {
-      throw new Error('Invalid data type. Currency must be an instance of Currency class.');
+  set amount(newAmount) {
+    if (typeof newAmount !== 'number') {
+      throw new TypeError('Amount must be a number');
     }
+    this._amount = newAmount;
   }
 
-  // Method to display attributes in the specified format
+  set currency(newCurrency) {
+    if (!(newCurrency instanceof Currency)) {
+      throw new TypeError('Currency must be a Currency');
+    }
+    this._currency = newCurrency;
+  }
+
   displayFullPrice() {
     return `${this._amount} ${this._currency.name} (${this._currency.code})`;
   }
 
-  // Static method to convert price based on conversion rate
   static convertPrice(amount, conversionRate) {
-    if (typeof amount === 'number' && typeof conversionRate === 'number') {
-      return amount * conversionRate;
-    } else {
-      throw new Error('Invalid data type. Amount and conversionRate must be numbers.');
-    }
+    return amount * conversionRate;
   }
 }
